fb94d829c57e1131d5061e8df0ced0e54d1525e8
alterquit <101753851+alterquit@users.noreply.github.com>
2023-04-10 19:38:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -8,56 +8,64 @@ from stats_report import StatsReport
 import sqlite3
 from validation import show_login
 
+
 class MainApp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        #Layout
-        self.geometry("600x350")
+        self.username = ''
+        # Layout
+        self.geometry("1050x900")
         self.title("Inventory management system")
         # Connect to the database
         self.conn = sqlite3.connect("inventory_management.db")
         self.user_type = None
 
         show_login(self)
-        
+
     # Global setting
     def start_app(self):
         self.container = tk.Frame(self)
         self.container.pack(side="top", fill="both", expand=True)
-
-        self.create_navigation_buttons(self.container)
 
         self.content_frame = tk.Frame(self.container)
         self.content_frame.pack(side="top", fill="both", expand=True)
 
         self.frames = {}
         for F in (Overview, SearchConfiguration, UserManagement, StatsReport):
-            frame = F(self.content_frame, self)
+            if F == Overview:
+                frame = F(self.content_frame, self, self.username)
+            else:
+                frame = F(self.content_frame, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
+        self.create_navigation_buttons(self.container)  # Move this line here
+
         self.show_frame(Overview)
-        
+
     # Set button
     def create_navigation_buttons(self, container):
         self.nav_frame = tk.Frame(container)
         self.nav_frame.pack(side="top", fill="x")
-        
+
         # Function button
         overview_button = tk.Button(self.nav_frame, text="Overview", command=lambda: self.show_frame(Overview))
         overview_button.pack(side="left")
 
-        search_config_button = tk.Button(self.nav_frame, text="Search & Configuration", command=lambda: self.show_frame(SearchConfiguration))
+        search_config_button = tk.Button(self.nav_frame, text="Search & Configuration",
+                                         command=lambda: self.show_frame(SearchConfiguration))
         search_config_button.pack(side="left")
-        
+
         # Administrator-specific button
         if self.user_type == "admin":
-            user_management_button = tk.Button(self.nav_frame, text="User Management", command=lambda: self.show_frame(UserManagement))
+            user_management_button = tk.Button(self.nav_frame, text="User Management",
+                                               command=lambda: self.show_frame(UserManagement))
             user_management_button.pack(side="left")
 
-            stats_report_button = tk.Button(self.nav_frame, text="Stats & Reports", command=lambda: self.show_frame(StatsReport))
+            stats_report_button = tk.Button(self.nav_frame, text="Stats & Reports",
+                                            command=lambda: self.show_frame(StatsReport))
             stats_report_button.pack(side="left")
-        
+
         # Logout and Exit button
         logout_button = tk.Button(self.nav_frame, text="Logout", command=self.logout)
         logout_button.pack(side="right")
